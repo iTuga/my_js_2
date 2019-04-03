c01ab56734f7277a95ca12653dafe837e25d01ef
Roman <ddaweedd@gmail.com>
2019-04-03 09:53:39 +0000

    Message

---
+++ answer1.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript?fbclid=IwAR3MUO-jL8ijcpow-BBVhKbEsskbswiTUUuOBeY5OKXlgoEnuQYAwvFoIG4
+
+function circleCircumference(circle) {
+return (circle.radius  * 2 * Math.PI);
+}
